03967dec6fb7972bc94bfc86ec43548911cfff4d
Santiago Barrera <santiago.barrerae@gmail.com>
2022-11-14 18:28:45 -0500

    se elminan contactos y tareas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { TasksModule } from './modules/tasks/tasks.module';
-import { ContactsModule } from './modules/contacts/contacts.module';
 
 @Module({
-  imports: [UsersModule, TasksModule, ContactsModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
